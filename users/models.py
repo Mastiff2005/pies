3f2c5106a142c28007821661d81f7cffc4395234
Mastiff2005 <pavel402@bk.ru>
2021-04-13 17:51:54 +0300

    homepage re-design

--- users/models.py
+++ users/models.py
@@ -4,12 +4,16 @@ from django.contrib.auth.models import User
 
 class UserProfile(User):
     organization = User.first_name
-    phone = models.SlugField(verbose_name='Телефон', max_length=50,
-                             help_text='Обязательное поле. Укажите телефон в формате 8-XXX-XXX-XX-XX.')
-    address = models.CharField(verbose_name='Адрес доставки', max_length=200, blank=True, null=True)
+    phone = models.SlugField(
+        verbose_name='Телефон', max_length=50,
+        help_text='Обязательное поле. Укажите телефон '
+                  'в формате 8-XXX-XXX-XX-XX.'
+        )
+    address = models.CharField(
+        verbose_name='Адрес доставки', max_length=200, blank=True, null=True)
 
     def __str__(self):
-       return self.username
+        return self.username
 
     class Meta:
         verbose_name = 'Профиль'
